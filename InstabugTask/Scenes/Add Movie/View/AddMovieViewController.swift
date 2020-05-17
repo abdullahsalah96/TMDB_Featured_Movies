9fd095a99d32ab459b274e2648450dbcd2fc5831
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2020-05-17 18:48:38 +0200

    add movie view setup

--- InstabugTask/Scenes/Add Movie/View/AddMovieViewController.swift
+++ InstabugTask/Scenes/Add Movie/View/AddMovieViewController.swift
@@ -8,12 +8,61 @@
 
 import UIKit
 
-class AddMovieViewController:
-
-
-UIViewController {
-
+class AddMovieViewController:UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var overviewTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var addMovieButton: UIButton!
+    // MARK: - Variables
+    private var imagePicker = UIImagePickerController()
+    private var Movie: Movie!
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpView()
     }
+    // MARK: - Set Up views
+    func setUpView(){
+        setUpAddMovieButton()
+        setUpImageView()
+    }
+    // MARK: - Add Movie button stylin
+    func setUpAddMovieButton(){
+        addMovieButton.layer.cornerRadius = 5
+        addMovieButton.backgroundColor = .systemTeal
+        addMovieButton.setTitleColor(.white, for: .normal)
+    }
+    //MARK: - Image View styling
+    func setUpImageView(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        posterImageView.isUserInteractionEnabled = true
+        posterImageView.addGestureRecognizer(tapGestureRecognizer)
+        posterImageView.layer.cornerRadius = 15
+    }
+    // MARK: - Add movie button pressed
+    @IBAction func addMoviePressed(_ sender: Any) {
+        //check that text fields are not empty
+    }
+    // MARK: - Image view is tapped, select image from gallery
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+}
+
+extension AddMovieViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //show image picker controller
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            posterImageView.image = image
+            posterImageView.contentMode = .scaleAspectFill
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+
 }
