e7fff938b2577eadccf1e77f9c4252e76d649d33
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2020-05-17 12:31:19 +0200

    Initial Commit

---
+++ InstabugTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstabugTask
+//
+//  Created by Abdalla Elshikh on 5/17/20.
+//  Copyright © 2020 Abdalla Elshikh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
