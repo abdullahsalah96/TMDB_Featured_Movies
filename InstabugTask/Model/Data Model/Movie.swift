f73f34dcf76d6766f9dc28e84ee1ae23ecc5c2eb
Abdalla Elshikh <43406571+abdullahsalah96@users.noreply.github.com>
2020-05-17 17:42:01 +0200

    Add movie view

--- InstabugTask/Model/Data Model/Movie.swift
+++ InstabugTask/Model/Data Model/Movie.swift
@@ -11,19 +11,19 @@ import UIKit
 
 struct Movie: Codable{
     let posterPath: String?
-    let adult: Bool
+    let adult: Bool?
     let overview: String
     let releaseDate: String
-    let genreIds: [Int]
-    let id: Int
-    let originalTitle: String
-    let originalLanguage: String
+    let genreIds: [Int]?
+    let id: Int?
+    let originalTitle: String?
+    let originalLanguage: String?
     let title: String
     let backdropPath: String?
-    let popularity: Double
-    let voteCount: Int
-    let video: Bool
-    let voteAverage: Double
+    let popularity: Double?
+    let voteCount: Int?
+    let video: Bool?
+    let voteAverage: Double?
     
     var releaseYear: String {
         return String(releaseDate.prefix(4))
---
+++ InstabugTask/Scenes/Add Movie/View/AddMovieViewController.swift
@@ -0,0 +1,19 @@
+//
+//  AddMovieViewController.swift
+//  InstabugTask
+//
+//  Created by Abdalla Elshikh on 5/17/20.
+//  Copyright © 2020 Abdalla Elshikh. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController:
+
+
+UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
